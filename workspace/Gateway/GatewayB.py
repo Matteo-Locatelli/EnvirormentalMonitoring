4cd4d959e49541927c26a94c1efee13a69741555
wasim-essbai <w.essbai@studenti.unibg.it>
2022-01-22 19:50:13 +0100

    deployment e component diagram fix

--- workspace/Gateway/GatewayB.py
+++ workspace/Gateway/GatewayB.py
@@ -14,7 +14,7 @@ server = "localhost:8080"
 dev_eui = bytes([0x0a, 0xc1, 0x4a, 0xad, 0x3e, 0x63, 0x91, 0xa1])
 
 # The API token (retrieved using the web-interface).
-api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiMzM4MzY2YTctNWExNy00ZWIxLTljZWMtNGM3MDY3NDExM2Y3IiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTY0MjgwMTQ1Mywic3ViIjoiYXBpX2tleSJ9.6XodcL1K4aHufV2c1y6zsTIgK3ddgSGo7DhSqy8aPp0"
+api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiNTFmNWVhMmItZGVkOC00ZjU2LTlkOGYtMTU0YTIwYjZiMDZmIiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTY0Mjg3MDU4Miwic3ViIjoiYXBpX2tleSJ9.cCLELlxRPbSk8YFJm4ndjxjssmAX4BKEizDKVp3TWpo"
 
 if __name__ == "__main__":
   # Connect without using TLS.
