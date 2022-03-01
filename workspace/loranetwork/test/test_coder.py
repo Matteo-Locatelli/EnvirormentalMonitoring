a2c6bde59dac6073d02facd2e3374ce7bdf67702
wasim-essbai <w.essbai@studenti.unibg.it>
2022-03-01 18:04:42 +0100

    fix a seguito di test

--- workspace/loranetwork/test/test_coder.py
+++ workspace/loranetwork/test/test_coder.py
@@ -121,19 +121,19 @@ class TestCoder(unittest.TestCase):
             "mic": "852bded1"
         }
 
-        self.jsons = [self.json0, self.json1, self.json2, self.json3]
+        self.json_list = [self.json0, self.json1, self.json2, self.json3]
 
     def tearDown(self):
         print("\n TearDown")
 
     # Comparison between payloads in the format of Base64
-    # encodePhyPayloadFromJSON: JSON -> Base64
+    # encode_phy_payload_from_json: JSON -> Base64
     def test_encode_phy_payload(self):
         print("\n test_encode_phy_payload")
-        self.assertEqual(self.payloads[0], coder.encode_phy_payload_from_json(self.jsons[0]))
-        self.assertEqual(self.payloads[1], coder.encode_phy_payload_from_json(self.jsons[1]))
-        self.assertEqual(self.payloads[2], coder.encode_phy_payload_from_json(self.jsons[2]))
-        self.assertEqual(self.payloads[3], coder.encode_phy_payload_from_json(self.jsons[3]))
+        self.assertEqual(self.payloads[0], coder.encode_phy_payload_from_json(self.json_list[0]))
+        self.assertEqual(self.payloads[1], coder.encode_phy_payload_from_json(self.json_list[1]))
+        self.assertEqual(self.payloads[2], coder.encode_phy_payload_from_json(self.json_list[2]))
+        self.assertEqual(self.payloads[3], coder.encode_phy_payload_from_json(self.json_list[3]))
 
     # Comparison between JSONs of the payloads
     # decode_phy_payload: Base64 -> PhyPayload
@@ -143,20 +143,20 @@ class TestCoder(unittest.TestCase):
     # JSON 1-3: differences in 'fOptsLen' and 'fOpts' and 'frmPayload'
     def test_decode_phy_payload(self):
         print("\n test_decodePhyPayloads")
-        self.assertNotEqual(payload_util.get_json_from_object(coder.decode_phy_payload(self.payloads[0])), self.jsons[0])
-        self.assertNotEqual(payload_util.get_json_from_object(coder.decode_phy_payload(self.payloads[1])), self.jsons[1])
-        self.assertNotEqual(payload_util.get_json_from_object(coder.decode_phy_payload(self.payloads[2])), self.jsons[2])
-        self.assertNotEqual(payload_util.get_json_from_object(coder.decode_phy_payload(self.payloads[3])), self.jsons[3])
+        self.assertNotEqual(payload_util.get_json_from_object(coder.decode_phy_payload(self.payloads[0])), self.json_list[0])
+        self.assertNotEqual(payload_util.get_json_from_object(coder.decode_phy_payload(self.payloads[1])), self.json_list[1])
+        self.assertNotEqual(payload_util.get_json_from_object(coder.decode_phy_payload(self.payloads[2])), self.json_list[2])
+        self.assertNotEqual(payload_util.get_json_from_object(coder.decode_phy_payload(self.payloads[3])), self.json_list[3])
 
     # Comparison between PhyPayloads
-    # decodePhyPayloads: Base64 -> PhyPayload
+    # decode_phy_payload: Base64 -> PhyPayload
     # get_phy_payload_from_json: JSON -> PhyPayload
     def test_phy_payload(self):
-        print("\n test_payPhyload")
-        self.assertEqual(coder.decode_phy_payload(self.payloads[0]), coder.get_phy_payload_from_json(self.jsons[0]))
-        self.assertEqual(coder.decode_phy_payload(self.payloads[1]), coder.get_phy_payload_from_json(self.jsons[1]))
-        self.assertEqual(coder.decode_phy_payload(self.payloads[2]), coder.get_phy_payload_from_json(self.jsons[2]))
-        self.assertEqual(coder.decode_phy_payload(self.payloads[3]), coder.get_phy_payload_from_json(self.jsons[3]))
+        print("\n decode_phy_payload")
+        self.assertEqual(coder.decode_phy_payload(self.payloads[0]), coder.get_phy_payload_from_json(self.json_list[0]))
+        self.assertEqual(coder.decode_phy_payload(self.payloads[1]), coder.get_phy_payload_from_json(self.json_list[1]))
+        self.assertEqual(coder.decode_phy_payload(self.payloads[2]), coder.get_phy_payload_from_json(self.json_list[2]))
+        self.assertEqual(coder.decode_phy_payload(self.payloads[3]), coder.get_phy_payload_from_json(self.json_list[3]))
 
 
 if __name__ == '__main__':
